ec90704e9e37fab865118dd23e024fc5998e2c68
fabriziofresco <57996075+fabriziofresco@users.noreply.github.com>
2020-06-26 10:40:30 +0100

    Create run_pipeline.sh

---
+++ demo/run_pipeline.sh
@@ -0,0 +1,3 @@
+#!/usr/local/rvm/bin/rvm-shell
+
+/usr/local/rvm/gems/ruby-2.7.0/bin/morpheus apps add fab-app-cli-02 --debug -b 2 -O App.0.servicePlan=403 -O App.0.rootVolume.datastoreId=2 -O App.0.networkInterface.networkId=network-1676 -N
