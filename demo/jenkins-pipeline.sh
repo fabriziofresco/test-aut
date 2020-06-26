64f60882ece1fc10f1998b44f8cb0eb142c069d2
fabriziofresco <57996075+fabriziofresco@users.noreply.github.com>
2020-06-26 13:58:25 +0100

    Update jenkins-pipeline.sh

--- demo/jenkins-pipeline.sh
+++ demo/jenkins-pipeline.sh
@@ -11,4 +11,3 @@ cd /usr/local/rvm/gems/ruby-2.7.0/bin
 ./morpheus remote add dev $1 --insecure --debug
 ./morpheus login -T $2 --insecure --debug 
 ./morpheus apps add jenkins-testing_vm --debug -b 2 -O App.0.servicePlan=403 -O App.0.rootVolume.datastoreId=2 -O App.0.networkInterface.networkId=network-1676 -N
-
