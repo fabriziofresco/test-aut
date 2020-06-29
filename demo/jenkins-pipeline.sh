00246c4aa8631af59ae2bb3810f66ad3f70b0e9a
fabriziofresco <57996075+fabriziofresco@users.noreply.github.com>
2020-06-29 09:54:07 +0100

    Update jenkins-pipeline.sh

--- demo/jenkins-pipeline.sh
+++ demo/jenkins-pipeline.sh
@@ -6,7 +6,7 @@ cd /usr/local/rvm/gems/ruby-2.7.0/bin
     -O App.0.layout=1178 \
     -O App.0.servicePlan=403 \
     -O App.0.rootVolume.datastoreId=2 \
-    -O App.0.networkInterface.networkId=network-1676 \
-    -O App.0.config.customOptions.databaseName=fabdb \
-    -O App.0.config.customOptions.databaseUserName=fabuser \
-    -O App.0.config.customOptions.databasePassword=Fabpw123 -N
+    -O App.0.networkInterface.networkId=network-1676 -N
+#    -O App.0.config.customOptions.databaseName=fabdb \
+#    -O App.0.config.customOptions.databaseUserName=fabuser \
+#    -O App.0.config.customOptions.databasePassword=Fabpw123 
