51a519c379b71018c6148a967f31c260e9a31c64
fabriziofresco <57996075+fabriziofresco@users.noreply.github.com>
2020-06-30 23:06:15 +0100

    Update jenkins-cleanup.sh

--- demo/jenkins-cleanup.sh
+++ demo/jenkins-cleanup.sh
@@ -1,4 +1,4 @@
-#!/usr/local/rvm/bin/rvm-shell -l
+#!/usr/local/rvm/bin/rvm-shell -l +x
 
 cd /usr/local/rvm/gems/ruby-2.7.0/bin
 ./morpheus apps remove jenkins-testing_vm --remove-instances on -y -f || true
