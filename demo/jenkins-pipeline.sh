b10e4b9dec4a2c6bdbd045800a538c6d1afb4b2f
fabriziofresco <57996075+fabriziofresco@users.noreply.github.com>
2020-06-30 15:42:25 +0100

    Update jenkins-pipeline.sh

--- demo/jenkins-pipeline.sh
+++ demo/jenkins-pipeline.sh
@@ -6,8 +6,4 @@ cd /usr/local/rvm/gems/ruby-2.7.0/bin
     -O App.0.layout=1178 \
     -O App.0.servicePlan=403 \
     -O App.0.rootVolume.datastoreId=2 \
-    -O App.0.networkInterface.networkId=network-1676 -N
-#    -O App.0.config.customOptions.databaseName=fabdb \
-#    -O App.0.config.customOptions.databaseUserName=fabuser \
-#    -O App.0.config.customOptions.databasePassword=Fabpw123 
-
+    -O App.0.networkInterface.networkId=network-1676 -N | grep '  ID: ' | while read garb ID; do echo $ID; done > ~/workspace/instance_id.txt
